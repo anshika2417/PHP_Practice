7369000ec1b45a3d2cdc6f6dc0d237ab59f9be28
anshika2417 <anshikakumari571@gmail.com>
2024-12-01 12:47:24 +0530

    Data Types

---
+++ DataTypes.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        //PHP has 8 primitive datatypes 
+           /* Integer 
+              Floating point or Float
+              String
+              Booleans
+              Array
+              Object
+              Resource
+              Null
+           */ 
+            
+
+        $age=21; //Integer
+        echo "My age is $age";
+
+        echo "<br>";
+
+        $price=21.98; //Float
+        echo "Price is {$price}";
+
+        echo "<br>";
+
+        $name = "Anshika"; //String
+        echo "My name is ".$name;
+
+        echo "<br>";
+
+        $is_allowed=true;
+        echo $is_allowed;
+
+        echo "<br>";
+
+        $x="Hello World";
+        $x=null;
+        echo $x;
+
+    ?>
+</body>
+</html>
